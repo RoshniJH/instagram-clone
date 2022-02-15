ab43d93b352d8eb924a16504e4a1c226da59a09f
RoshniJH <jahirrose488@gmail.com>
2022-02-15 19:52:47 +0530

    Added Image, Username, and Caption to Instagram Post...and rendered multiple posts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-
+import React,{useState} from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const [posts,setPosts] = useState([
+    {
+      username:"Roshni" ,
+      caption:"wohooo" ,
+      imageUrl:"https://reactjs.org/logo-og.png"
+    },
+    {
+      username:"its_me",
+      caption:"tadaa",
+      imageUrl:"https://www.fnp.com/images/pr/x/v20210907192911/led-teddy-bear_1.jpg"
+    }
+  ]);
+  
   return (
     <div className="app">
       
       <div className="app__header">
-      <img 
-      className="app__headerImage"
-      src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-      alt ="" />
+            <img 
+            className="app__headerImage"
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt ="" />
       </div>
       <h1>instagram clone</h1>
-      <Post />
+      {
+        posts.map(post => {
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+      })}
+      <Post username="Roshni" caption="wohoo" imageUrl="https://reactjs.org/logo-og.png"/>
+      <Post username="its_me" caption="tadaa" imageUrl="https://i.ytimg.com/vi/XOPYNKvc4R0/maxresdefault.jpg"/>
      {/*header */}
      {/*posts */}
      {/*posts */}
--- src/Post.js
+++ src/Post.js
@@ -2,28 +2,28 @@ import React from 'react'
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username,caption,imageUrl}) {
   return (
     <div className="post">
-    <div className="post__header">
-    {/*header -> avatar + username} */}
-    <Avatar 
-    className="post__avatar"
-    alt='Roshna'
-    src="/static/images/avatar/1.jpg"
-    />
-    <h3>Username</h3>
-</div>
+      <div className="post__header">
+            {/*header -> avatar + username} */}
+            <Avatar 
+            className="post__avatar"
+            alt='Roshna'
+            src="/static/images/avatar/1.jpg"
+            />
+            <h3>{username}</h3>
+      </div>
 {/*image -> to post */}
 <img 
 className="post__image"
-src="https://reactjs.org/logo-og.png"
+src={imageUrl}
 alt="post"
 />
 
 {/* image bottom -> username + caption */}
 <h4 className="post__text">
-<strong>Roshna</strong> wohoo</h4>
+<strong>{username}</strong> {caption}</h4>
 
 </div>
   )
